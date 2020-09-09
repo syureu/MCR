300924387581e36e79951b0d5dd07b28e4a2912a
허진기 <god85a@naver.com>
2020-09-09 17:34:02 +0900

    review list 추가

--- frontend/src/views/feed/FeedDetail.vue
+++ frontend/src/views/feed/FeedDetail.vue
@@ -63,11 +63,11 @@
 			<a href="#" class="button">Read More</a>
 		</div>
 	</div>
+	
 </div>
-<div id="welcome" class="container">
-	<h2>Welcome to our website</h2>
-	<p>This is <strong>Porphyrio</strong>, a free, fully standards-compliant CSS template designed by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>. The photos in this template are from <a href="http://fotogrph.com/"> Fotogrph</a>. This free template is released under the <a href="http://templated.co/license">Creative Commons Attribution</a> license, so you're pretty much free to do whatever you want with it (even use it commercially) provided you give us credit for it. Have fun :) </p>		
-</div>
+<reviewWrite />
+<reviewList />
+
 <div id="copyright" class="container">
 	<p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
 </div>
@@ -87,10 +87,14 @@
 <script src="https://unpkg.com/vuewordcloud"></script>
 <script>
 import VueWordCloud from 'vuewordcloud';
+import reviewWrite from '@/components/review/ReviewWrite.vue';
+import reviewList from '@/components/review/ReviewList.vue';
 export default {
 	
 	components: {
     [VueWordCloud.name]: VueWordCloud,
+	reviewWrite,
+	reviewList,
   },
   data() {
       return {
