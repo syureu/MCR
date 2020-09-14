b80ef5419e15ded23b587eecec4778bd3765afc3
박현수 <p1230411@naver.com>
2020-09-14 17:31:23 +0900

    signup 2/3

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -1,6 +1,6 @@
-const IP ="http://192.168.199"
+const IP ="http://192.168.219.137"
 export default{
-    BASE_URL: `${IP}:8090`,
+    BASE_URL: `${IP}:8080`,
     IMG_URL: `${IP}:3000`,
     JSON_HEADER:{
         headers: {
