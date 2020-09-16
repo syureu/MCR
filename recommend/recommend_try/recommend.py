5f0bf42b45e780b24f573540b98136e07fb11e16
Syureu <tor01237@gmail.com>
2020-09-16 09:59:45 +0900

    S03P22D104-26 // content based filtering // description

--- recommend/recommend_try/recommend.py
+++ recommend/recommend_try/recommend.py
@@ -116,7 +116,50 @@ print()
 print("CBF")
 print()
 
-cbf = pd.read_csv('../resources/tmdb_movies_202009151544.csv')
-#cbf = cbf[cbf['tmdbId'].notnull()]['tmdbId'].astype('int')
+mydata = pd.read_csv('../resources/tmdb_movies_202009151556.csv')
+mydataid = mydata[mydata['id'].notnull()]['id'].astype('int')
 
-print(cbf)
+mymd = mydata
+mymd['id'] = mydata['id'].astype('int')
+smd = mymd[mymd['id'].isin(mydataid)]
+
+print(smd.shape)
+print(smd)
+print()
+
+smd['tagline'] = smd['tagline'].fillna('')
+smd['description'] = smd['overview'] + smd['tagline']
+smd['description'] = smd['description'].fillna('')
+
+print(smd)
+print()
+
+tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 2),
+                     min_df=0, stop_words='english')
+tfidf_matrix = tf.fit_transform(smd['description'])
+
+print(tfidf_matrix.shape)
+print(tfidf_matrix)
+print()
+
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+print(cosine_sim[0])
+
+smd = smd.reset_index()
+titles = smd['title']
+indices = pd.Series(smd.index, index=smd['title'])
+
+
+def get_recommendations(title):
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:31]
+    movie_indices = [i[0] for i in sim_scores]
+    return titles.iloc[movie_indices]
+
+
+print(get_recommendations('The Godfather').head(10))
+print()
+print(get_recommendations('Batman Returns').head(10))
+print()
