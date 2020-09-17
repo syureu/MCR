9864d987a2bbd49bdc693bf6907e61c01a4351a7
Syureu <tor01237@gmail.com>
2020-09-17 15:24:29 +0900

    S03P22D104-82 // simple recommend로 테스트 부터

---
+++ recommend/recommend_try/simple_recommend.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+from ast import literal_eval
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import wordnet
+
+#from surprise import Reader, Dataset, SVD, evaluate
+
+# pip install scikit-surpirse
+from surprise import Reader, Dataset, SVD
+
+from surprise.model_selection import cross_validate
+
+#import warnings; warnings.simplefilter('ignore')
+
+md = pd.read_json('../resources/tmdb_movies_202009151556.csv')
+
+
+def build_chart(genre, percentile=0.85):
+    df = md[md['genre'] == genre]
+    vote_counts = df[df['vote_count'].notnull()]['vote_count'].astype('int')
+    vote_averages = df[df['vote_average'].notnull()
+                       ]['vote_average'].astype('int')
+    C = vote_averages.mean()
+    m = vote_counts.quantile(percentile)
+
+    qualified = df[(df['vote_count'] >= m) & (df['vote_count'].notnull()) & (
+        df['vote_average'].notnull())][['title', 'year', 'vote_count', 'vote_average', 'popularity']]
+    qualified['vote_count'] = qualified['vote_count'].astype('int')
+    qualified['vote_average'] = qualified['vote_average'].astype('int')
+    qualified['wr'] = qualified.apply(lambda x: (
+        x['vote_count']/(x['vote_count']+m) * x['vote_average']) + (m/(m+x['vote_count']) * C), axis=1)
+    qualified = qualified.sort_values('wr', ascending=False).head(250)
+    return qualified
+
+
+print(build_chart('Science Fiction').head(15))
