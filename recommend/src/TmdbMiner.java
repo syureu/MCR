9c305ded4b7a4763ad047dec687d6f20fcf9b048
Syureu <tor01237@gmail.com>
2020-09-21 13:39:22 +0900

    S03P23D104-7 // Open Api 따오기

--- recommend/src/TmdbMiner.java
+++ recommend/src/TmdbMiner.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-public class Miner {
+public class TmdbMiner {
     public static void main(String[] args) throws Exception {
         int count = 1;
         String inline;
