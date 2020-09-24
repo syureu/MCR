0b9afe1f8f650a13780301320713901f48aad199
허진기 <god85a@naver.com>
2020-09-24 17:23:36 +0900

    review design 변경 및 작성 추가

--- frontend/src/components/review/ReviewWrite.vue
+++ frontend/src/components/review/ReviewWrite.vue
@@ -7,31 +7,40 @@
         <label id="lab" for="" >내평점</label> <button id="togglebtn" @click="changetoggle">{{ name }}</button>
         </div>
         <div id="incontainer" v-if="toggle==true" >
-        <StarRating v-model="rating" v-bind:max-rating="10" :show-rating="temp"/>
-        <textarea id="revcontent"></textarea>
-        <button id="revbutton" >작성</button>
+        <StarRating v-model="revitem.rate" v-bind:max-rating="10" :show-rating="temp"/>
+        <textarea id="revcontent" v-model="revitem.content"></textarea>
+        <button v-if="this.revcheck == true" class="revbutton" >수정</button>
+        <button v-else class="revbutton" @click="writerev">작성</button>
         </div>
         </div>
     </div>
 </template>
 
 <script>
-// import HTTP from "@/util/http-common.js"
-// import axios from 'axios'
+import URL from "@/util/http-common.js"
+import axios from 'axios'
 import StarRating from 'vue-star-rating';
 export default {
     name: 'ReviewWrite',
     components: {
         StarRating,
     },
+    props: ['movieNo'],
     data(){
         return {
             revwrite: null,
             temp:false,
-            rating:"",
             toggle: false,
             name: "펼치기",
             onlogin:false,
+            revcheck: false,
+            movieId: this.movieNo,
+            revitem: {
+                writer: "",
+                content: "",
+                rate: 0,
+                movieId: this.movieNo,
+            }
         }
     },
     methods:{
@@ -42,14 +51,52 @@ export default {
             } else {
                 this.name = "펼치기";
             }
+        },
+        writerev() {
+            axios.post(`${URL.BASE_URL}/mcr/daumreview/`, this.revitem)
+            .then(res => {
+                console.log("리뷰 작성 완료")
+                console.log(res)
+            })
         }
     },
     created() {
+        console.log(this.$store.getters.getUserData.userinfo)
         if(this.$store.getters.getUserData == null){
             this.onlogin = false;
         } else{
             this.onlogin = true;
         }
+
+        let daumReview = {
+            movieId: this.movieId,
+            writer: this.$store.getters.getUserData.userinfo.userid,
+            
+        }   
+
+        this.revitem.writer= this.$store.getters.getUserData.userinfo.userid;
+
+        console.log(daumReview)
+
+        axios.post(`${URL.BASE_URL}/mcr/daumreview/check`, daumReview)  
+        .then(res => {
+            console.log(res)
+            if(res.data.object != null){
+                this.revcheck = true;
+                console.log("res 확인")
+                console.log(res)
+                this.revitem = {
+                    content: res.data.object.content,
+                    rate: res.data.object.rate,
+                    movieId: res.data.object.movieId,
+                }
+                console.log("res 확인")
+                console.log(res)
+            } 
+        })
+        .catch(err => {
+            console.log(err)
+        })
     }
  }
 </script>
@@ -83,7 +130,7 @@ export default {
     #incontainer{
         border: 2px solid skyblue;
         margin : 2vh 2vw 2vh 2vw; 
-        padding : 2vh 2vw 2vh 2vw;
+        padding : 3vh 3vw 3vh 1.5vw;
     }
     #revcontainer{
         background-color : #fff9f7;
@@ -109,12 +156,17 @@ export default {
         padding-top: 1vh;
         color: black;
     }
-    #revbutton{
-        margin-top : 6vh;
-        margin-left: 2vw;
+    .revbutton{
+        margin-top : 15vh;
+        margin-left: 1vw;
+        margin-right: 2vw;
         font-size: 1.5vw ;
         position: absolute;
-            
+        background-color:#fff9f7;
+        -webkit-transition-duration: 0.4s; /* Safari */
+        transition-duration: 0.4s;
+        border: 0;
+        outline: 0;
     }
 
     #lab{
